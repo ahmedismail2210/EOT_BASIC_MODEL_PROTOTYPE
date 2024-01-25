c5e3c33bb7e12a73ac4d9ba016d98195e7fabd4c
Replit user <>
2024-01-25 16:33:26 +0000

    Reset Password section added with full functionality

--- reviewmodel/urls.py
+++ reviewmodel/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from webapp2 import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,22 @@ urlpatterns = [
     path('profile-update/', views.userUpdate, name='update_profile'),
     path('login/', views.Login, name='login'),
     path('logout/', views.Logout, name='logout'),
+    path('reset_password/',
+         auth_view.PasswordResetView.as_view(
+             template_name='reset_password.html'),
+         name='reset_password'),
+    path('reset_password_done/',
+         auth_view.PasswordResetDoneView.as_view(
+             template_name='reset_password_done.html'),
+         name='password_reset_done'),
+    path('reset_password_confirm/<uidb64>/<token>/',
+         auth_view.PasswordResetConfirmView.as_view(
+             template_name='reset_password_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_view.PasswordResetCompleteView.as_view(
+             template_name='reset_password_complete.html'),
+         name='password_reset_complete'),
     path('add-property/', views.createProject, name='property'),
     path('profile/', views.single_profile, name='profile'),
     path('signup/', views.registerUser, name='register'),
